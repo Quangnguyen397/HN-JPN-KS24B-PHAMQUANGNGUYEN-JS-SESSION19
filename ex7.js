a53bc081b934c47bee527723b501506b95585768
Quangnguyen397 <qp601697@gmail.com>
2025-03-03 10:44:06 +0700

    upload

--- ex7.js
+++ ex7.js
@@ -1,5 +1,5 @@
 let a = Number(prompt("moi ban nhap vao so a:"));
 let b = Number(prompt("moi ban nhap vao so b:"));
-let c = Number(prompt("moi ban nhap vao so b:"));
-let max = Math.max(a, b, c);
-alert("so lon nhat trong 3 so la:" + max);
+let c = Number(prompt("moi ban nhap vao so c:"));
+let max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+alert("so lon nhat trong 3 so la: " + max);
